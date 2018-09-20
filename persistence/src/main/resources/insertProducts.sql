f53078c75b66738e19141dfc536e0e869a6aa494
nclozza <nclozza@itba.edu.ar>
2018-09-20 16:53:57 -0300

    Changed SQL scripts to create the tables and insert fake data

--- persistence/src/main/resources/insertProducts.sql
+++ persistence/src/main/resources/insertProducts.sql
@@ -1,58 +1,68 @@
-INSERT INTO products VALUES('iPhone X', 'Apple', '3GB', '256GB', 
-	'iOS 11.1.1', 'A11 Bionic', '143.6 x 70.9 x 7.7 mm', '84.4 cm2', 
-	'19.5:9', '12 MP, f/1.8, 28mm, 1.22µm, OIS, PDAF --- 
-	12 MP, f/2.4, 52mm, 1.0µm, OIS, PDAF, 2x optical zoom', 
+INSERT INTO products (productname, brand, ram, storage, operativesystem, processor, bodysize, screensize, screenratio, rearcamera, frontcamera)
+VALUES('iPhone X', 'Apple', '3GB', '256GB',
+	'iOS 11.1.1', 'A11 Bionic', '143.6 x 70.9 x 7.7 mm', '84.4 cm2',
+	'19.5:9', '12 MP, f/1.8, 28mm, 1.22µm, OIS, PDAF ---
+	12 MP, f/2.4, 52mm, 1.0µm, OIS, PDAF, 2x optical zoom',
 	'7 MP, f/2.2, 32mm');
 
-INSERT INTO products VALUES('iPhone 8', 'Apple', '2GB', '256GB', 
+INSERT INTO products (productname, brand, ram, storage, operativesystem, processor, bodysize, screensize, screenratio, rearcamera, frontcamera)
+VALUES('iPhone 8', 'Apple', '2GB', '256GB',
 	'iOS 11', 'A11 Bionic', '138.4 x 67.3 x 7.3 mm', '60.9 cm2',
 	'16:9', '12 MP, f/1.8, 28mm, OIS, PDAF', '7 MP, f/2.2');
 
-INSERT INTO products VALUES('iPhone 8 Plus', 'Apple', '3GB', '256GB', 
+INSERT INTO products (productname, brand, ram, storage, operativesystem, processor, bodysize, screensize, screenratio, rearcamera, frontcamera)
+VALUES('iPhone 8 Plus', 'Apple', '3GB', '256GB',
 	'iOS 11', 'A11 Bionic', '158.4 x 78.1 x 7.5 mm', '83.4 cm2', '16:9',
-	'12 MP, f/1.8, 28mm, OIS, PDAF --- 
+	'12 MP, f/1.8, 28mm, OIS, PDAF ---
 	12 MP, f/2.8, 57mm, 2x optical zoom, PDAF', '7 MP, f/2.2, 32mm');
 
-INSERT INTO products VALUES('OnePlus 6', 'OnePlus', '8GB', '128GB', 
-	'Android 8.1 (Oreo)', 'Qualcomm Snapdragon 845', 
-	'155.7 x 75.4 x 7.8 mm', '98.4 cm2', '19:9', 
-	'16 MP, f/1.7, 25mm, 1/2.6", 1.22µm, OIS, PDAF --- 
-	20 MP (16 MP effective), f/1.7, 25mm, 1/2.8", 1.0µm, PDAF', 
+INSERT INTO products (productname, brand, ram, storage, operativesystem, processor, bodysize, screensize, screenratio, rearcamera, frontcamera)
+VALUES('OnePlus 6', 'OnePlus', '8GB', '128GB',
+	'Android 8.1 (Oreo)', 'Qualcomm Snapdragon 845',
+	'155.7 x 75.4 x 7.8 mm', '98.4 cm2', '19:9',
+	'16 MP, f/1.7, 25mm, 1/2.6", 1.22µm, OIS, PDAF ---
+	20 MP (16 MP effective), f/1.7, 25mm, 1/2.8", 1.0µm, PDAF',
 	'16 MP, f/2.0, 25mm, 1/3", 1.0µm');
 
-INSERT INTO products VALUES('Galaxy S8', 'Samsung', '4GB', '64GB',
-	'Android 7.0 (Nougat)', 'Qualcomm Snapdragon 835', 
-	'148.9 x 68.1 x 8 mm', '84.8 cm2', '18.5:9', 
+INSERT INTO products (productname, brand, ram, storage, operativesystem, processor, bodysize, screensize, screenratio, rearcamera, frontcamera)
+VALUES('Galaxy S8', 'Samsung', '4GB', '64GB',
+	'Android 7.0 (Nougat)', 'Qualcomm Snapdragon 835',
+	'148.9 x 68.1 x 8 mm', '84.8 cm2', '18.5:9',
 	'12 MP, f/1.7, 26mm, 1/2.5", 1.4µm, OIS, dual pixel PDAF',
 	'8 MP, f/1.7, 25mm, 1/3.6", 1.22µm, AF');
 
-INSERT INTO products VALUES('Galaxy S8+', 'Samsung', '6GB', '128GB',
-	'Android 7.0 (Nougat)', 'Qualcomm Snapdragon 835', 
+INSERT INTO products (productname, brand, ram, storage, operativesystem, processor, bodysize, screensize, screenratio, rearcamera, frontcamera)
+VALUES('Galaxy S8+', 'Samsung', '6GB', '128GB',
+	'Android 7.0 (Nougat)', 'Qualcomm Snapdragon 835',
 	'159.5 x 73.4 x 8.1 mm', '98.3 cm2', '18.5:9',
 	'12 MP, f/1.7, 26mm, 1/2.5", 1.4µm, OIS, dual pixel PDAF',
 	'8 MP, f/1.7, 25mm, 1/3.6", 1.22µm, AF');
 
-INSERT INTO products VALUES('Galaxy S9', 'Samsung', '4GB', '256GB', 
+INSERT INTO products (productname, brand, ram, storage, operativesystem, processor, bodysize, screensize, screenratio, rearcamera, frontcamera)
+VALUES('Galaxy S9', 'Samsung', '4GB', '256GB',
 	'Android 8.0 (Oreo)', 'Qualcomm Snapdragon 845', '147.7 x 68.7 x 8.5 mm',
-	'84.8 cm2', '18.5:9', 
+	'84.8 cm2', '18.5:9',
 	'12 MP, f/1.5-2.4, 26mm, 1/2.5", 1.4µm, OIS, dual pixel PDAF',
 	'8 MP, f/1.7, 25mm, 1/3.6", 1.22µm, AF');
 
-INSERT INTO products VALUES('Galaxy S9+', 'Samsung', '6GB', '256GB', 
+INSERT INTO products (productname, brand, ram, storage, operativesystem, processor, bodysize, screensize, screenratio, rearcamera, frontcamera)
+VALUES('Galaxy S9+', 'Samsung', '6GB', '256GB',
 	'Android 8.0 (Oreo)', 'Qualcomm Snapdragon 845', '158.1 x 73.8 x 8.5 mm',
-	'98.3 cm2', '18.5:9', 
-	'12 MP, f/1.5-2.4, 26mm, 1/2.55", 1.4µm, Dual Pixel PDAF, OIS --- 
+	'98.3 cm2', '18.5:9',
+	'12 MP, f/1.5-2.4, 26mm, 1/2.55", 1.4µm, Dual Pixel PDAF, OIS ---
 	12 MP, f/2.4, 52mm, 1/3.6", 1µm, AF, OIS, 2x optical zoom',
 	'8 MP, f/1.7, 25mm, 1/3.6", 1.22µm, AF');
 
-INSERT INTO products VALUES('Pocophone F1', 'Xiaomi', '8GB', '256GB', 
+INSERT INTO products (productname, brand, ram, storage, operativesystem, processor, bodysize, screensize, screenratio, rearcamera, frontcamera)
+VALUES('Pocophone F1', 'Xiaomi', '8GB', '256GB',
 	'Android 8.1 (Oreo)', 'Qualcomm Snapdragon 845', '155.5 x 75.3 x 8.8 mm',
 	'96.2 cm2', '18.7:9', '12 MP, f/1.9, 1/2.55", 1.4µm, dual pixel PDAF ---
 	5 MP, f/2.0, 1.12µm, depth sensor', '20 MP, f/2.0, 0.9µm');
 
-INSERT INTO products VALUES('P20 Pro', 'Huawei', '8GB', '256GB', 
+INSERT INTO products (productname, brand, ram, storage, operativesystem, processor, bodysize, screensize, screenratio, rearcamera, frontcamera)
+VALUES('P20 Pro', 'Huawei', '8GB', '256GB',
 	'Android 8.1 (Oreo)', 'Qualcomm Snapdragon 845', '155 x 73.9 x 7.8 mm',
 	'93.9 cm2', '18.7:9', '40 MP, f/1.8, 27mm, 1/1.7", OIS, PDAF/Laser AF ---
 	20 MP B/W, f/1.6, 27mm, 1/2.7", OIS, PDAF/Laser AF ---
-	8 MP, f/2.4, 80mm, 1/4", 3x optical zoom, OIS, PDAF/Laser AF', 
+	8 MP, f/2.4, 80mm, 1/4", 3x optical zoom, OIS, PDAF/Laser AF',
 	'24 MP, f/2.0, 26mm');
